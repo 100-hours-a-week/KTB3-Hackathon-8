d4a21501dbc18487c9a33cd1aa710f4d793c2fbe
codeok2 <bumseok970305@gmail.com>
2025-12-18 13:48:43 +0900

    fix: Exception Handler 처리할 예정이므로 Controller 에서 Exception 처리 제거

--- app/backend/src/main/java/com/ktb/user/controller/UserController.java
+++ app/backend/src/main/java/com/ktb/user/controller/UserController.java
@@ -2,33 +2,29 @@ package com.ktb.user.controller;
 
 import com.ktb.user.dto.UserDto;
 import com.ktb.user.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.net.URI;
 
 @RestController
 @RequestMapping("/api/v1/users")
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
-    // 회원 가입
-    @PostMapping
-    public ResponseEntity<UserDto> signup(@RequestBody UserDto userDto) {
 
-        try {
-            userService.create(userDto);
-            return ResponseEntity.status(HttpStatus.CREATED).body(userDto);
-        } catch (Exception e) {
-            throw new IllegalArgumentException("중복된 아이디입니다.");
-        }
+    @PostMapping
+    @Operation(summary = "회원 가입")
+    @ApiResponse(responseCode = "200", description = "회원 가입 성공")
+    public ResponseEntity<UserDto> signup(@RequestBody UserDto userDto) {
+        userService.create(userDto);
+        return ResponseEntity.ok().body(userDto);
     }
 
-    // 로그인
 
 }
