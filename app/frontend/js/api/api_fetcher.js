be96d5111e7734efac882dd52619eb34f3cc3627
Jimin Kim <0305jimin@gmail.com>
2025-12-19 02:03:26 +0900

    feat: using csrf token

--- app/frontend/js/api/api_fetcher.js
+++ app/frontend/js/api/api_fetcher.js
@@ -1,24 +1,53 @@
+async function fetchCsrfToken() {
+    if (csrfTokenCache) return csrfTokenCache;
+
+    const res = await fetch(`${API_BASE_URL}/csrf`, { credentials: 'include' });
+    const data = await res.json();
+
+    csrfTokenCache = data.token;
+
+    return csrfTokenCache;
+}
+
 export async function fetchAPIWithBody(url, method, body) {
+    const token = await fetchCsrfToken();
+    const headers = new Headers(options.headers || {});
+
+    headers.set('Content-Type', 'application/json');
+    headers.set('X-XSRF-TOKEN', token);
+
     return await fetch(url, {
                         method: method,
-                        headers: { 'Content-Type': 'application/json' },
+                        headers,
                         body: body,
                         credentials: 'include'
                     });
 }
 
 export async function fetchAPIWithFile(url, method, formData) {
+    const token = await fetchCsrfToken();
+    const headers = new Headers(options.headers || {});
+
+    headers.set('X-XSRF-TOKEN', token);
+
     return await fetch(url, {
                         method: method,
                         body: formData,
+                        headers,
                         credentials: 'include'
                     });
 }
 
 export async function fetchAPI(url, method) {
+    const token = await fetchCsrfToken();
+    const headers = new Headers(options.headers || {});
+
+    headers.set('Content-Type', 'application/json');
+    headers.set('X-XSRF-TOKEN', token);
+
     return await fetch(url, {
                         method: method,
-                        headers: { 'Content-Type': 'application/json' },
+                        headers,
                         credentials: 'include'
                     });
 }
