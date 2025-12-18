e60da8a5f4d6e6a85aeaf6f97440ba41fdc825ff
mdgome <mdgome3@gmail.com>
2025-12-18 21:12:02 +0900

    fix: swagger 접근 불가로 수정 진행

---
+++ app/backend/src/main/java/com/ktb/config/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package com.ktb.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Yogiyo Boss API")
+                        .description("KTB Hackathon Project - Group Delivery Service API")
+                        .version("v1.0.0"))
+                .servers(List.of(
+                        new Server()
+                                .url("http://localhost:8080")
+                                .description("Development Server")
+                ));
+    }
+}
