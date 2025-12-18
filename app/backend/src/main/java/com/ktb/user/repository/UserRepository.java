42dbc5c04d874c1419de99f979b2121ec65c99d6
mdgome <mdgome3@gmail.com>
2025-12-18 17:46:48 +0900

    style: 오타 수정

--- app/backend/src/main/java/com/ktb/user/repository/UserRepository.java
+++ app/backend/src/main/java/com/ktb/user/repository/UserRepository.java
@@ -10,5 +10,5 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<UserIdentifier, Long> {
     Optional<UserIdentifier> findByUsername(String username);
 
-    Optional<Object> findUserIdentifierByNickName(String userNickname);
+    Optional<Object> findUserIdentifierByNickname(String userNickname);
 }
